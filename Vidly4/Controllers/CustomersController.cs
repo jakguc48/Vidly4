99725bbeab18344760bc08b220a0eb8fd4c45f42
Jakub Gucwa <jakub.gucwa@outlook.com>
2018-08-10 19:39:35 +0200

    Dodano do widoku customerwyswietlenie znizki do kazdego klienta i okreslenie. zmieniono rowniez wyglad tabeli

--- Vidly4/Controllers/CustomersController.cs
+++ Vidly4/Controllers/CustomersController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 using System.Web;
 using System.Web.Mvc;
 using Vidly4.Models;
@@ -31,7 +32,9 @@ namespace Vidly4.Controllers
         {
             //180810_1_18:51 tutaj zmieniamy na odniesienie do kontekstu zamiast wewnętrznej metody. Odniesienie do db
             //dodajemy do odniesienia TOLIST żeby od razu ładował całość a nie pojedynczo podczas iteracji
-            var Customer = _context.Customers.ToList();
+                //180810_1_19:36 dodajemy include, zeby w podowaniu zawrzec rowniez inna tabele, aby sie do niej odwlolac potem
+            var Customer = _context.Customers.Include(c => c.MembershipType).ToList();
+            //180810_1_19:36----------------------------------------------------------------------------------------------------
             return View(Customer);
             //180810_1_18:51----------------------------------------------------------------------------------------------------
         }
